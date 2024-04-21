70d9e33e83665f1d3dbbb21fb1908ee972ffd73b
Shawn Catanzarite <me@shawncatz.com>
2024-04-21 17:22:46 +0000

    styling

--- ui/src/components/email/body.tsx
+++ ui/src/components/email/body.tsx
@@ -3,7 +3,7 @@ import { Email } from 'client';
 import { Paper } from '@mui/material';
 
 export const EmailBody = ({ email }: { email: Email }) => {
-  if (email.body_html !== undefined) {
+  if (email.body_html && email.body_html !== '') {
     return (
       <Paper elevation={1} sx={{ p: 2 }}>
         <div dangerouslySetInnerHTML={{ __html: email.body_html }} />
@@ -12,8 +12,18 @@ export const EmailBody = ({ email }: { email: Email }) => {
   }
 
   return (
-    <div>
-      <pre>{email.body}</pre>
-    </div>
+    <Paper
+      elevation={1}
+      sx={{
+        p: 2,
+        '& .email-plain': {
+          fontSize: '0.75rem',
+          whiteSpace: 'pre-wrap',
+          wordWrap: 'break-word',
+        },
+      }}
+    >
+      <pre className="email-plain">{email.body}</pre>
+    </Paper>
   );
 };
